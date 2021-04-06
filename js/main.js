a3e68c1d15b312b3a2807677e71bab12242439e6
R1d22 <rydsic@gmail.com>
2021-04-06 13:35:35 +0300

    lvl2

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-/*let elInputFName = document.querySelector('.f-name');
-let elInputLName = document.querySelector('.l-name');
-let elInputMName = document.querySelector('.m-name');
-let elBtnSubm = document.querySelector('.btn-subm');
-let elFNameRes = document.querySelector('.f-name__res');
-let elLNameRes = document.querySelector('.l-name__res');
-let elMNameRes = document.querySelector('.m-name__res');
-
-
-elBtnSubm.addEventListener('click', (ev) => {
-   // let elFNameRes = elInputFName.Value;
-    elFNameRes.innerHTML = elInputFName.target.value;
-    elLNameRes.innerHTML = elInputLName;
-    elMNameRes.innerHTML = elInputMName;
-});*/
-
-
 const form = document.forms.visitCard;
 const res = document.querySelector('.res')
 
@@ -22,16 +5,20 @@ form.addEventListener ('submit', (ev) => {
     ev.preventDefault();
 
     const formData = new FormData (ev.target);
-    const FName = formData.get('f-name');
-    const LName = formData.get('l-name');
-    const MName = formData.get('m-name');
-    //const submit = formData.get('submit');
+    const fName = formData.get('f-name');
+    const lName = formData.get('l-name');
+    const mName = formData.get('m-name');
 
 
     const html = `
         <div>
-            <div>Firstname: ${FName}</div>
-            <div>Lastmane: ${LName}</div>
-            <div>Midlname: ${MName}</div>
+            <div>Firstname: ${fName}</div>
+            <div>Lastmane: ${lName}</div>
+            <div>Midlname: ${mName}</div>
         </div>
     `;
+    
+
+    res.insertAdjacentHTML('afterBegin', html);
+
+});
